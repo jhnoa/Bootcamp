ceee759507e8b9446f65627c8eddc849bc771300
Johan <Me@Aviano.local>
2018-07-16 11:09:33 +0700

    Day13 Done

--- Day13/src/main.js
+++ Day13/src/main.js
@@ -1,11 +1,17 @@
 // @flow
-import React from 'react';
-import App from './App';
-import {render} from 'react-dom';
+import fs from 'fs';
+let readdir = (fileName: string) => {
+  return new Promise((resolve, reject) => {
+    fs.readdir(fileName, (error, result) => {
+      if (error) {
+        reject(error);
+      }
+      resolve(result);
+    });
+  });
+};
 
-let container = document.createElement('div');
-if (document.body) {
-  document.body.appendChild(container);
-}
-
-render(<App />, container);
+let testPromise = readdir('../');
+testPromise.then((result) => {
+  console.log(result);
+});
