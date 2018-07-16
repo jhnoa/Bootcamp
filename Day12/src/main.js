41ef6feaff780728b192a35bef72d830016b624c
Johan <Me@Aviano.local>
2018-07-16 10:49:18 +0700

    clear day 12 and init day 13

--- Day12/src/main.js
+++ Day12/src/main.js
@@ -1,11 +1,58 @@
 // @flow
-import React from 'react';
-import App from './App';
-import {render} from 'react-dom';
 
-let container = document.createElement('div');
-if (document.body) {
-  document.body.appendChild(container);
-}
+import fetch from 'node-fetch';
 
-render(<App />, container);
+let doFetch = (url: string): Promise<any> => {
+  console.log('doFetch');
+  return fetch(url);
+};
+
+let git: Promise<Response> = fetch(
+  'https://api.github.com/users/mozilla/repos',
+);
+let subscribersPromise = git
+  .then((gitHeader) => {
+    return gitHeader.json();
+  })
+  .then((gitBody: Array<{subscribers_url: string}>) => {
+    let subscribersURL: Array<string> = [];
+
+    for (let url of gitBody) {
+      subscribersURL.push(url.subscribers_url);
+    }
+    return subscribersURL;
+  })
+  .then((urlList) => {
+    let urlPromise: Array<Promise<*>> = [];
+    for (let url of urlList) {
+      urlPromise.push(doFetch(url));
+    }
+    return urlPromise;
+  })
+  .then((urlPromise) => {
+    return Promise.all(urlPromise).then((arrayContent: Array<Object>) => {
+      let subscribersContentPromiseArray: Array<Object> = [];
+      for (let index of arrayContent.keys()) {
+        subscribersContentPromiseArray.push(arrayContent[index].json());
+      }
+      return subscribersContentPromiseArray;
+    });
+  })
+  .then((subscribersContentPromiseArray) => {
+    return Promise.all(subscribersContentPromiseArray).then(
+      (arrayArrayContent: Array<Array<{login: string}>>) => {
+        console.log(arrayArrayContent);
+        let bigArray = 0;
+        for (let arrayContent of arrayArrayContent) {
+          bigArray += arrayContent.length;
+        }
+        return bigArray;
+      },
+    );
+  })
+  .then((numb) => {
+    console.log(numb);
+  })
+  .catch((error) => {
+    console.log(error);
+  });
